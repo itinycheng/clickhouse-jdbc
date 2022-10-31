c15ed78c420fc6666ab396934eda087e69b32cc6
chenglong.wang <tiny.wcl@gmail.com>
2022-10-31 19:47:13 +0800

    Add toString method to ClickHouseArray for plaintext data display in hue

--- clickhouse-jdbc/src/main/java/ru/yandex/clickhouse/ClickHouseArray.java
+++ clickhouse-jdbc/src/main/java/ru/yandex/clickhouse/ClickHouseArray.java
@@ -1,12 +1,14 @@
 package ru.yandex.clickhouse;
 
+import ru.yandex.clickhouse.domain.ClickHouseDataType;
+
 import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
+import java.util.Arrays;
 import java.util.Map;
-
-import ru.yandex.clickhouse.domain.ClickHouseDataType;
+import java.util.Objects;
 
 /**
  * @author Dmitry Andreev <a href="mailto:AndreevDm@yandex-team.ru"></a>
@@ -39,7 +41,7 @@ public class ClickHouseArray implements Array {
 
     @Override
     public Object getArray() throws SQLException {
-        if (array == null){
+        if (array == null) {
             throw new SQLException("Call after free");
         }
         return array;
@@ -83,5 +85,19 @@ public class ClickHouseArray implements Array {
     @Override
     public void free() throws SQLException {
         array = null;
+    }
+
+    @Override
+    public String toString() {
+        if (!array.getClass().isArray()) {
+            return Objects.toString(array);
+        }
+
+        int arrayLen = java.lang.reflect.Array.getLength(array);
+        Object[] newObjArray = new Object[arrayLen];
+        for (int i = 0; i < arrayLen; i++) {
+            newObjArray[i] = java.lang.reflect.Array.get(array, i);
+        }
+        return Arrays.toString(newObjArray);
     }
 }
